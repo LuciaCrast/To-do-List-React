0b580aa96a15a9928ce0e774c05b23f98c6ee12e
Lucia Cano <luciacanoras@gmail.com>
2021-11-30 12:42:45 +0100

    Add App hooks and functions

---
+++ src/components/App.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { NewTask } from "./NewTask";
+import { TasksList } from "./TasksList";
+
+function App() {
+  const [newTask, setNewTask] = useState({});
+  const [allTasks, setAllTask] = useState([]);
+
+  function handleChange({ target }) {
+    const { name, value } = tart;
+    setNewTask((prevState) => ({
+      ...prevState,
+      [name]: value,
+      id: Date.now(),
+    }));
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (!newTask) return;
+    setAllTask((prevState) => [newTask, ...prevState]);
+    setNewTask({});
+  }
+
+  function handleDelete(taskIdToRemove) {
+    setAllTask((prevState) =>
+      prevState.filter((task) => task.id !== taskIdToRemove)
+    );
+  }
+
+  return (
+    <main>
+      <h1>Tasks</h1>
+      <NewTask
+        newTask={newTask}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+      />
+      <TasksList allTasks={allTasks} handleDelete={handleDelete} />
+    </main>
+  );
+}
+
+export default App;
