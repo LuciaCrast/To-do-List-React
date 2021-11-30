09bb8824e1bdadbda81fa0ae592d28a1decfb72e
Lucia Cano <luciacanoras@gmail.com>
2021-11-30 12:41:53 +0100

    Add TaskList component

---
+++ src/components/TasksList.jsx
@@ -0,0 +1,16 @@
+function TasksList({ allTasks, handleDelete }) {
+  return (
+    <ul>
+      {allTasks.map(({ title, description, id }) => (
+        <li key={id}>
+          <div>
+            <h2>{title}</h2>
+            <button onClick={() => handleDelete(id)}>X</button>
+          </div>
+          {!description ? null : <p>{description}</p>}
+        </li>
+      ))}
+    </ul>
+  );
+}
+export { TasksList };
