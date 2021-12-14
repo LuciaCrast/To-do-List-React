eb2749702f75ae3bf4af786ec0c3333c01c3be12
Lucia Cano <luciacanoras@gmail.com>
2021-12-14 11:21:27 +0100

    Fix map at TaskList component

--- src/components/TasksList.jsx
+++ src/components/TasksList.jsx
@@ -1,13 +1,19 @@
+import "./tasklist.scss";
+
 function TasksList({ allTasks, handleDelete }) {
   return (
-    <ul>
+    <ul className="list">
       {allTasks.map(({ title, description, id }) => (
-        <li key={id}>
-          <div>
-            <h2>{title}</h2>
-            <button onClick={() => handleDelete(id)}>X</button>
+        <li className="list__li" key={id}>
+          <div className="list__container">
+            <h2 className="list__title">{title}</h2>
+            <button className="list__xbutton" onClick={() => handleDelete(id)}>
+              X
+            </button>
           </div>
-          {!description ? null : <p>{description}</p>}
+          {!description ? null : (
+            <p className="list__description">{description}</p>
+          )}
         </li>
       ))}
     </ul>
